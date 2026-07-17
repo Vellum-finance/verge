43a64da17ae82054e96b7b460f465b6c473d7f0a
Vellum <new.project.deploy@gmail.com>
2026-07-17 09:55:41 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,11 +13,11 @@ function VergeApp() {
    <div className="min-h-screen bg-background text-foreground relative overflow-hidden">
      
      <div className="absolute inset-0 pointer-events-none overflow-hidden">
-       <div className="absolute -top-40 left-1/2 -translate-x-1/2 h-[600px] w-[900px] bg-brand/15 blur-[160px] rounded-full" />
+       <div className="absolute -top-40 left-1/2 -translate-x-1/2 h-[600px] w-[900px] bg-brand/15 blur-[160px] rounded-full glow-move" />
 
-       <div className="absolute top-20 -left-40 h-[500px] w-[500px] bg-brand/10 blur-[150px] rounded-full" />
+       <div className="absolute top-20 -left-40 h-[500px] w-[500px] bg-brand/10 blur-[150px] rounded-full glow-left" />
 
-       <div className="absolute top-20 -right-40 h-[500px] w-[500px] bg-brand/10 blur-[150px] rounded-full" />
+       <div className="absolute top-20 -right-40 h-[500px] w-[500px] bg-brand/10 blur-[150px] rounded-full glow-right" />
      </div>
 
      <div className="absolute inset-0 opacity-[0.03] pointer-events-none"
